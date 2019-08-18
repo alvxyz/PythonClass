3c5bcdf29ef5383d1c6e6438a4496259d83605af
alvxyz <ayamgoreng1887>
2019-08-19 05:33:18 +0700

    Menambahkan komentar

--- Struktur Data (Pak Bowo)/Program Semester/Photoshop.py
+++ Struktur Data (Pak Bowo)/Program Semester/Photoshop.py
@@ -1,43 +1,45 @@
 class Stack:
+    # variabel local
     stack_list = []
     stack_redo = []
     max_size = 0
     top = 0
 
+    # konstruktor dengan parameter size
     def __init__(self, size):
         self.max_size = size
-        self.stack_list = [" "] * self.max_size
+        self.stack_list = [0] * self.max_size
+        # nilai top -1 dikarenakan slot tumpukan pertama [indeks 0} belum di isi
         self.top = -1
 
     def push(self, perintah):
-        # if not self.is_full():
+        # nilai top bergeser, seiiring dengan dimasukkan data baru
         self.top += 1
         self.stack_list[self.top] = perintah
 
     def pop(self):
         temp = self.stack_list[self.top]
+        # nilai top berkurang
         self.top -= 1
         return temp
 
     def is_empty(self):
+        # melakukan cek apakah nilai top seperti keadaan pertama (sebelum data masuk)
         return self.top == -1
 
     def is_full(self):
+        # melakukan cek apakah nilai top sama dengan nilai maksimum (dikurangi 1 karena indeks mulai dari 0)
         return self.top == self.max_size - 1
 
     def peek_top(self):
+        # mengembalikan nilai pada list yang berada di indeks top
         return self.stack_list[self.top]
 
     def histori(self):
         print("History: ")
+        # melakukan perulangan untuk menampilkan isi list (top + 1 dikrenanan aturan dalam range)
         for i in range(0, self.top + 1):
-            print(self.stack_list[i].capitalize(), end=" ")
-        print()
-
-    def redo(self):
-        temp = self.stack_list[self.top + 1]
-        self.top += 1
-        return temp
+            print("-", self.stack_list[i].capitalize())
 
 
 import time
@@ -62,7 +64,7 @@ def mainprogram():
     - Quit = Keluar dari program
     """)
 
-    while not quit:
+    while not Quit:
 
         perintah = input("Photoshop [CC 2019] > ")
 
